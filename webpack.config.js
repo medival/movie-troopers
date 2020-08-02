04f6d53ed57ad080bd1c52ac0fac6e3b3ceb74da
Adi Purnomo <adipurnomo811@gmail.com>
2020-08-02 21:36:18 +0700

    checkpoint-before add hovercss

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,39 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const webpack = require("webpack");
 const path = require("path");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-	entry: "./src/js/index.js",
-	output: {
-		path: path.resolve(__dirname, "dist"),
-		filename: "index.js",
-	},
-	mode: "production",
-	module: {
-		rules: [
-			{
-				test: /\.css$/,
-				use: ["style-loader", "css-loader"],
-			},
-			{
-				test: /bootstrap\/dist\/js\/umd\//,
-				use: "imports-loader?jQuery=jquery",
-			},
-		],
-	},
-	plugins: [
-		// provide jQuery and Popper.js dependencies
-		new webpack.ProvidePlugin({
-			$: "jquery",
-			jQuery: "jquery",
-			jquery: "jquery",
-			"window.jQuery": "jquery",
-			Popper: ["popper.js", "default"],
-		}),
-		new HtmlWebpackPlugin({
-			template: path.resolve(__dirname, "src/index.html"),
-			filename: "index.html",
-		}),
-	],
+    entry: "./src/js/index.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "index.js",
+    },
+    mode: "production",
+    module: {
+        rules: [{
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+            },
+            {
+                test: /bootstrap\/dist\/js\/umd\//,
+                use: "imports-loader?jQuery=jquery",
+            },
+        ],
+    },
+    plugins: [
+        // provide jQuery and Popper.js dependencies
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery",
+            jquery: "jquery",
+            "window.jQuery": "jquery",
+            Popper: ["popper.js", "default"],
+        }),
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, "src/index.html"),
+            filename: "index.html",
+        }),
+        new MiniCssExtractPlugin()
+    ],
 };
